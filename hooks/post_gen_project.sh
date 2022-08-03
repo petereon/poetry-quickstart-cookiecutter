b2b4709b9434184fba902ed4c688eefad65d4a6e
Peter Vyboch <peter.vyboch@ibm.com>
2022-08-03 09:18:48 +0200

    feat: lots of automatization

---
+++ hooks/post_gen_project.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Initialize as git directory
+git --version && git init
+
+# Initialize pre-commit hooks
+poetry --version && poetry install && poetry run pre-commit --version && 
+poetry run pre-commit install --install-hooks -t commit-msg -t pre-push -t pre-commit
+
+ln -s ./scripts/post-commit .git/hooks/post-commit
