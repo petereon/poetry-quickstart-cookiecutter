6aa3ffe7598dafd63634eaf336f688e604cd4486
Peter Vyboch <peter.vyboch@ibm.com>
2022-08-04 13:11:45 +0200

    fix: post commit

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -7,5 +7,5 @@ git --version && git init
 poetry --version && poetry install && poetry run pre-commit --version && 
 poetry run pre-commit install --install-hooks -t commit-msg -t pre-push -t pre-commit
 
-echo "post-commit installed at ./git/hooks/post-commit" mv ./scripts/post-commit .git/hooks/post-commit
-chmod 775 .git/hooks/post-commit
+# echo "post-commit installed at ./git/hooks/post-commit" mv ./scripts/post-commit .git/hooks/post-commit
+# chmod 775 .git/hooks/post-commit
